bb77df5fce0754b56033e2f79a61a512dedf518a
nguyentrung56789 <47946738+nguyentrung56789@users.noreply.github.com>
2025-10-24 20:12:19 +0700

    Delete check_in directory

--- check_in/api/internal_key.js
+++
@@ -1,4 +0,0 @@
-// ======================== internal_key.js ========================
-
-// 1) Khóa nội bộ để gửi vào header x-internal-key
-window.getInternalKey = () => "Trung@123"; // đổi nếu bạn đặt giá trị khác trên server
