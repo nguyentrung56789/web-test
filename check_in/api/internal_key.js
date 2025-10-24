22a159d147b6ed7f8f15e7a43ccf5e4b762c19a1
nguyentrung56789 <47946738+nguyentrung56789@users.noreply.github.com>
2025-10-24 20:10:27 +0700

    Add files via upload

---
+++ check_in/api/internal_key.js
@@ -0,0 +1,4 @@
+// ======================== internal_key.js ========================
+
+// 1) Khóa nội bộ để gửi vào header x-internal-key
+window.getInternalKey = () => "Trung@123"; // đổi nếu bạn đặt giá trị khác trên server
